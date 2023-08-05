57abdbc391f701b1fc67754b0d230e90d68a92dc
kjy <nashirah23@gmail.com>
2023-08-05 15:48:44 +0900

    hello world

--- src/main/java/com/miner/jpa/HelloWorldController.java
+++ src/main/java/com/miner/jpa/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.miner.jpa;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "hello world";
+    }
 }
